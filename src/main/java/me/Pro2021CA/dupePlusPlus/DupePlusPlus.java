d03a98800e5b80daccc39afe256b39c859852bf7
Pro2021CA <ceesaugusteijn@gmail.com>
2026-01-09 21:49:41 +0100

    DupePlusPlus v2.0

---
+++ src/main/java/me/Pro2021CA/dupePlusPlus/DupePlusPlus.java
@@ -0,0 +1,38 @@
+package me.Pro2021CA.dupePlusPlus;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static me.Pro2021CA.dupePlusPlus.blacklist.blacklisteditems;
+
+public final class DupePlusPlus extends JavaPlugin {
+    public static Plugin plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+        if (getConfig().getList("blacklisted items") != null) {
+            blacklisteditems = (List<ItemStack>) getConfig().getList("blacklisted items");
+        } else {
+            blacklisteditems = new ArrayList<ItemStack>();
+            // Plugin startup logic
+        }
+        getCommand("blacklist").setExecutor(new blacklist());
+        getCommand("dupe").setExecutor(new dupe());
+        getCommand("nondupeable").setExecutor(new persistendata());
+        getCommand("maxdupe").setExecutor(new maxdupe());
+    }
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        plugin.getConfig().set("blacklisted items", blacklisteditems);
+        saveConfig();
+    }
+}
