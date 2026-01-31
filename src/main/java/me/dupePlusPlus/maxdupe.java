4619fc2c5952865ac944adb31e248f6f2795c4a6
Pro2021CA <221412200+Pro2021CA@users.noreply.github.com>
2026-01-31 10:59:49 +0100

    Improved readability and fixed some data issues

---
+++ src/main/java/me/dupePlusPlus/maxdupe.java
@@ -0,0 +1,30 @@
+package me.Pro2021CA.dupePlusPlus;
+
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginLogger;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.logging.Level;
+
+public class maxdupe implements CommandExecutor {
+    public static int MaxDupe;
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+        if (strings.length == 0){
+            PluginLogger pluginLogger = new PluginLogger(DupePlusPlus.plugin);
+            pluginLogger.log(Level.WARNING, "NO ARGUMENTS FOUND! PLEASE ADD AN ARGUMENT");
+            return true;
+        }
+        if (commandSender instanceof Player p){
+            p.sendMessage(MiniMessage.miniMessage().deserialize(DupePlusPlus.plugin.getConfig().getString("prefix") + "Max dupe set"));
+        }
+        DupePlusPlus.plugin.getConfig().set("maxdupe", Integer.parseInt(strings[0]));
+        DupePlusPlus.plugin.saveConfig();
+        MaxDupe = Integer.parseInt(strings[0]);
+        return true;
+    }
+}
